abdc61eacc056472dbb40271ebc852a11be012c4
chimpa117829 <115164081+chimpa117829@users.noreply.github.com>
2023-03-28 13:06:47 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log
+let personajes = [
+    {
+        id:1,
+        nombre: "Dai",
+        descripcion: "hijo de un caballero dragon y una humana",
+        images: "./src/images/Dai.jpg"
+    }
+    {}
+]
